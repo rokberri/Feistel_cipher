a19887f8ad59bd8f8a7be2eb85d568c3defa1795
rokberri <rokberri@gmail.com>
2023-09-18 03:44:16 +0300

    Added OFB mode

--- feistel_net.py
+++ feistel_net.py
@@ -3,8 +3,24 @@ from utils.io import *
 from math import ceil
 
 class Feistel_Net:
+    
+    def encode(self,path, secret_key, block_size, amount_of_rounds,f1,f2, mode='default'):
+        
+        if mode == 'default':
+            return self.encode_default(path, secret_key, block_size, amount_of_rounds,f1,f2)
+        elif mode == 'OFB':
+            return self.encode_OFB(path, secret_key, block_size, amount_of_rounds,f1,f2)
 
-    def encode(self,path, secret_key, block_size, amount_of_rounds,f1,f2)->bytes:
+    def decode(self,path, secret_key, block_size, amount_of_rounds,f1,f2, mode='default'):
+        
+        if mode == 'default':
+            return self.decode_default(path, secret_key, block_size, amount_of_rounds,f1,f2)
+        elif mode == 'OFB':
+            return self.decode_OFB(path, secret_key, block_size, amount_of_rounds,f1,f2)
+        
+        
+        
+    def encode_default(self,path, secret_key, block_size, amount_of_rounds,f1,f2):
         
         secret_text = ''
         #считываем всю информацию для кодирования
@@ -33,13 +49,8 @@ class Feistel_Net:
                 block = b2 + b3 + b1 + b0  
             secret_text += block
         print(f'SECRET_TEXT:\n{secret_text}')
-        
-        
-        
-        
-        
-        
-    def decode(self, path, secret_key, block_size, amount_of_rounds,f1,f2):
+                   
+    def decode_default(self, path, secret_key, block_size, amount_of_rounds,f1,f2):
         secret_text = ''
         #считываем всю информацию для кодирования
         data_to_encode = read_data_from_file(path)
@@ -63,10 +74,71 @@ class Feistel_Net:
                 b2 = XOR(b2,NOT(round_keys[round_num]))
                 b3 = XOR(b3, f1(b0,b1))
                 b0 = XOR(b0, round_keys[round_num])
-                print(b0,'>>>',)
                 # ///
                 block = b0 + b1 + b2 + b3  
             secret_text += block
+            # print(f'ClEAN BLOCK:{block}')
+
+        print(f'CLEAN DATA:\n{secret_text}')
+        
+    def encode_OFB(self,path, secret_key, block_size, amount_of_rounds,f1,f2):
+        iv = '1111000110100110011101001011101101010100000110010110011110101100' 
+        secret_text = ''
+        #считываем всю информацию для кодирования
+        data_to_encode = get_data_for_encode(path)
+        print(f'CLEAN DATA:\n{data_to_encode}')
+        # делим ее на блоки
+        blocks = self.split_data_into_blocks(data_to_encode,block_size)
+        # #генерируем раундовые ключи, переписать метод(генераторная функция?)
+        round_keys = self.generate_round_keys(secret_key, amount_of_rounds)
+
+        # # запускаем цикл для каждого блока 
+        for block in blocks:
+            # для каждого блока запускаем установленное кол-во раундов шифрования
+            for round_num in range(amount_of_rounds):
+                b0 = iv[0:16]
+                b1 = iv[16:32]
+                b2 = iv[32:48]
+                b3 = iv[48:64]
+                # doing something with blocks
+                # ///
+                b0 = XOR(b0,round_keys[round_num])
+                b3 = XOR(b3,f1(b0,b1))
+                b2 = XOR(b2,NOT(round_keys[round_num]))
+                b1 = XOR(b1,(f2(b2,b3)))
+                # ///
+                iv = b2 + b3 + b1 + b0  
+            secret_text += XOR(block, iv)
+        print(f'SECRET_TEXT:\n{secret_text}')
+    
+    
+    def decode_OFB(self, path, secret_key, block_size, amount_of_rounds,f1,f2):
+        iv = '1111000110100110011101001011101101010100000110010110011110101100'
+        secret_text = ''
+        #считываем всю информацию для кодирования
+        data_to_encode = read_data_from_file(path)
+        print(f'SECRET_TEXT:\n{data_to_encode}')
+        # делим ее на блоки
+        blocks = self.split_data_into_blocks(data_to_encode,block_size)
+        # #генерируем раундовые ключи
+        round_keys = self.generate_round_keys(secret_key, amount_of_rounds)
+        # # запускаем цикл для каждого блока 
+        for block in blocks:
+            # для каждого блока запускаем установленное кол-во раундов шифрования
+            for round_num in range(amount_of_rounds):
+                b0 = iv[0:16]
+                b1 = iv[16:32]
+                b2 = iv[32:48]
+                b3 = iv[48:64]
+                # doing something with blocks
+                # ///
+                b0 = XOR(b0,round_keys[round_num])
+                b3 = XOR(b3,f1(b0,b1))
+                b2 = XOR(b2,NOT(round_keys[round_num]))
+                b1 = XOR(b1,(f2(b2,b3)))
+                # ///
+                iv = b2 + b3 + b1 + b0 
+            secret_text += XOR(block,iv)
             # print(f'ClEAN BLOCK:{block}')
 
         print(f'CLEAN DATA:\n{secret_text}')
--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def F2(a:str,b:str)->str:
 if __name__ == '__main__':
     SUPER_DUPER_SECRET_KEY = '1110010110001111110000101101001001101111000000110111110111110111'
     feistel_net = Feistel_Net()
-    feistel_net.encode('OpenText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2)
-    feistel_net.decode('SecretText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2)
+    # feistel_net.encode('OpenText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2)
+    # feistel_net.decode('SecretText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2)
+    print('----------------------------------------------------------------------')
+    feistel_net.encode('OpenText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2,'OFB')
+    feistel_net.decode('SecretText.txt',SUPER_DUPER_SECRET_KEY,64,1,F1,F2,'OFB')
     
     
